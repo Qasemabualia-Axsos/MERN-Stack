83c25c56fc265b06dcb44df930dda4f7a780e58a
qassim aboalia <qqq144992@gmail.com>
2025-12-16 16:38:07 +0200

    Todo-List

---
+++ React.js/train/src/components/Result.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+const Result=(props)=>{
+
+    return (
+        props.tabs.map((item,index)=>{
+            return(
+                <button key={index}  onClick={()=>props.onTabClick(item)}>{item}</button>
+            )
+        })
+    )
+}
+
+
+export default Result
